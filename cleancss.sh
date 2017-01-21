2df696d91fed92c17254ee901bca6c72b5d477ae
Eric Workman <eworkman@covermymeds.com>
2017-01-21 11:52:35 -0500

    Clean up style

---
+++ cleancss.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+./node_modules/.bin/cleancss -o assets/css/icon.min.css assets/css/icon.css
+./node_modules/.bin/cleancss -o assets/css/oscar.min.css assets/css/oscar.css
+./node_modules/.bin/cleancss -o assets/css/photoswipe.min.css assets/css/photoswipe.css
+./node_modules/.bin/cleancss -o assets/css/prism.min.css assets/css/prism.css
+./node_modules/.bin/cleancss -o assets/css/poole.min.css assets/css/poole.css
+
